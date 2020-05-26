0d281b66a7b3e13ea2f123a71a12960677536188
ryota <sigurebidesu@yahoo.co.jp>
2020-05-26 23:19:00 +0900

    お問い合わせフォーム完成前

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
+import Vue        from 'vue'
+import App        from './App.vue'
+import router     from './router/index'
 import titleMixin from './util/title'
-import axios from 'axios'
+import axios      from 'axios'
 
 Vue.mixin(titleMixin)
 Vue.prototype.$axios = axios
