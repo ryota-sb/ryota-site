6b7cf60a8b938f144492ffdb4a15ee65b5fb0ed0
ryota <sigurebidesu@yahoo.co.jp>
2020-05-27 16:21:24 +0900

    Vuetify Form 雛形

--- src/Views/ContactPage.vue
+++ src/Views/ContactPage.vue
@@ -4,12 +4,6 @@
       <h1>Contact</h1>
       <p>- お問い合わせ -</p>
     </div>
-    <form id="contact_form">
-      <input v-model="name" type="text" placeholder="お名前">
-      <input v-model="email" type="text" placeholder="メールアドレス">
-      <textarea v-model="content" placeholder="お問い合わせ内容"></textarea>
-      <input type="button" value="送信" v-on:click="send">
-    </form>
     <div>
       <contact-page-form />
     </div>
@@ -26,25 +20,7 @@ export default {
       content: ''
     }
   },
-  components: { ContactPageForm },
-  methods: {
-    send() {
-      const params = {
-        name: this.name,
-        email: this.email,
-        content: this.content
-      }
-      const apiUrl = 'https://7rpd3dcicb.execute-api.ap-northeast-1.amazonaws.com/v1/sendmail'
-      this.$axios.post(apiUrl, params).then(response => {
-        console.log(response.data);
-        this.name = '';
-        this.email = '';
-        this.content = '';
-      }).catch(error => {
-        console.log(error);
-      })
-    }
-  }
+  components: { ContactPageForm }
 }
 </script>
 
@@ -55,38 +31,6 @@ export default {
       h1 {
         color: white;
         -webkit-text-stroke: 1px black;
-      }
-    }
-    form {
-      margin: 0 auto;
-      width: 300px;
-      input {
-        margin-bottom: 15px;
-        padding-left: 10px;
-        width: 100%;
-        height: 30px;
-        outline: none;
-      }
-      textarea {
-        margin-bottom: 10px;
-        padding-top: 10px;
-        padding-left: 10px;
-        width: 100%;
-        height: 250px;
-        outline: none;
-        resize: none;
-      }
-    }
-  }
-
-  @media screen and (max-width: 420px) {
-    .contact-page {
-      form {
-        width: auto;
-        input, textarea {
-          width: 90%;
-          -webkit-appearance: none;
-        }
       }
     }
   }
--- src/components/ContactPageForm.vue
+++ src/components/ContactPageForm.vue
@@ -1,17 +1,42 @@
 <template>
   <v-app>
     <v-container>
-      <v-row>
-        <v-col>
-          <v-card>
-            <v-form>
-              <v-text-field
-                v-model="name"
-                outlined
-              >
-              </v-text-field>
-            </v-form>
-          </v-card>
+      <v-row justify="center">
+        <v-col cols="10" md="6">
+          <v-form>
+            <v-text-field
+              v-model="name"
+              label="お名前"
+              color="#7CC6CF"
+              outlined
+              dense
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="email"
+              label="メールアドレス"
+              color="#7CC6CF"
+              outlined
+              dense
+            >
+            </v-text-field>
+            <v-textarea
+              v-model="content"
+              label="内容"
+              color="#7CC6CF"
+              outlined
+              dense
+            >
+            </v-textarea>
+            <v-btn
+              block
+              outlined
+              color="#7CC6CF"
+              @click="send"
+            >
+              送信
+            </v-btn>
+          </v-form>
         </v-col>
       </v-row>
     </v-container>
@@ -22,7 +47,27 @@
 export default {
   data() {
     return {
-      name: 'aaa'
+      name: '',
+      email: '',
+      content: ''
+    }
+  },
+  methods: {
+    send() {
+      const params = {
+        name: this.name,
+        email: this.email,
+        content: this.content
+      }
+      const apiUrl = 'https://7rpd3dcicb.execute-api.ap-northeast-1.amazonaws.com/v1/sendmail'
+      this.$axios.post(apiUrl, params).then(response => {
+        console.log(response.data);
+        this.name = '';
+        this.email = '';
+        this.content = '';
+      }).catch(error => {
+        console.log(error);
+      })
     }
   }
 }
