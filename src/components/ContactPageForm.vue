dd16d07ba947c02be0407d045e9d813cea6940f7
ryota <sigurebidesu@yahoo.co.jp>
2020-05-27 17:49:58 +0900

    Form validate

--- src/components/ContactPageForm.vue
+++ src/components/ContactPageForm.vue
@@ -3,11 +3,16 @@
     <v-container>
       <v-row justify="center">
         <v-col cols="10" md="6">
-          <v-form>
+          <v-form
+            v-model="valid"
+            ref="valid_form"
+          >
             <v-text-field
               v-model="name"
               label="お名前"
               color="#7CC6CF"
+              :rules="nameRules"
+              :counter="10"
               outlined
               dense
             >
@@ -16,6 +21,7 @@
               v-model="email"
               label="メールアドレス"
               color="#7CC6CF"
+              :rules="emailRules"
               outlined
               dense
             >
@@ -24,6 +30,8 @@
               v-model="content"
               label="内容"
               color="#7CC6CF"
+              :rules="contentRules"
+              :counter="100"
               outlined
               dense
             >
@@ -31,7 +39,7 @@
             <v-btn
               block
               outlined
-              color="#7CC6CF"
+              color="#800080"
               @click="send"
             >
               送信
@@ -49,7 +57,20 @@ export default {
     return {
       name: '',
       email: '',
-      content: ''
+      content: '',
+      valid: true,
+      nameRules: [
+        v => !!v || '名前を入力してください',
+        v => v.length <= 10 || '名前は10文字以内で入力してください'
+      ],
+      emailRules: [
+        v => !!v || 'メールアドレスを入力してください',
+        v => /.+@.+\..+/.test(v) || 'メールアドレスの形式が正しくありません'
+      ],
+      contentRules: [
+        v => !!v || 'お問い合わせ内容を入力してください',
+        v => v.length <= 100 || '100文字以内で入力してください'
+      ]
     }
   },
   methods: {
@@ -60,14 +81,17 @@ export default {
         content: this.content
       }
       const apiUrl = 'https://7rpd3dcicb.execute-api.ap-northeast-1.amazonaws.com/v1/sendmail'
-      this.$axios.post(apiUrl, params).then(response => {
-        console.log(response.data);
-        this.name = '';
-        this.email = '';
-        this.content = '';
-      }).catch(error => {
-        console.log(error);
-      })
+      
+      if (this.$refs.valid_form.validate()) {
+        this.$axios.post(apiUrl, params).then(response => {
+          console.log(response.data)
+          this.name = '';
+          this.email = '';
+          this.content = '';
+        }).catch(error => {
+          console.log(error);
+        })
+      }
     }
   }
 }
