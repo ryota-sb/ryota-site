881480959f4e6d8af8c0f112d3ca15b55caf73a0
ryota <sigurebidesu@yahoo.co.jp>
2020-05-28 20:25:09 +0900

    footer snsアイコン

--- src/components/share/Footer.vue
+++ src/components/share/Footer.vue
@@ -1,5 +1,17 @@
 <template>
   <footer>
+    <div class="icons">
+      <div class="twitter">
+        <a href="#">
+          <font-awesome-icon :icon="['fab', 'twitter']" />
+        </a>
+      </div>
+      <div class="github">
+        <a href="https://github.com/ryota-sb?tab=repositories">
+          <font-awesome-icon :icon="['fab', 'github']" />
+        </a>
+      </div>
+    </div>
     <p>©2020 Ryota.</p>
   </footer>
 </template>
@@ -7,12 +19,35 @@
 <style lang="scss">
   footer {
     margin-top: auto;
-    height: 30px;
     line-height: 30px;
     text-align: center;
     border-top: 1px solid purple;
     p {
       color: purple;
+    }
+    .icons {
+      margin-top: 10px;
+      text-align: rigth;
+      .twitter,
+      .github {
+        display: inline-block;
+        a {
+          display: block;
+          svg {
+            margin: 5px;
+            padding: 2px; 
+            width: 26px;
+            height: 26px;
+            border: 1px solid black;
+            border-radius: 50%;
+            color: purple;
+            &:hover {
+            border: 1px solid plum;
+            color: plum;
+            }
+          }
+        }
+      }
     }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import axios      from 'axios'
 import Vuetify    from 'vuetify'
 import '../node_modules/vuetify/dist/vuetify.min.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faGithub, faTwitter)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.use(Vuetify)
 const vuetify = new Vuetify()
 Vue.mixin(titleMixin)
