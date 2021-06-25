b45d7531926b6bcd416741ef4073fe2603c35064
ryota-sb <ryota.sb.1206@gmail.com>
2021-06-25 18:58:13 +0900

    ルート追加”

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ import SkillPage     from '@/Views/SkillPage'
 import PortfolioPage from '@/Views/PortfolioPage'
 import ContactPage   from '@/Views/ContactPage'
 
+import BookShelf               from '@/Views/portfolio/BookShelf'
+import RyotaSite               from '@/Views/portfolio/RyotaSite'
+import SpotifyDiscoverPlaylist from '@/Views/portfolio/SpotifyDiscoverPlaylist'
+import FargateEnv              from '@/Views/portfolio/FargateEnv'
+
 Vue.use(Router);
 
 export default new Router({
@@ -16,5 +21,9 @@ export default new Router({
     { path: '/skill',     name: 'skill-page',     component: SkillPage },
     { path: '/portfolio', name: 'portfolio-page', component: PortfolioPage },
     { path: '/contact',   name: 'contact-page',   component: ContactPage },
+    { path: '/portfolio/bookshelf',               name: 'bookshelf-page',               component: BookShelf },
+    { path: '/portfolio/ryotasite',               name: 'ryotasite-page',               component: RyotaSite },
+    { path: '/portfolio/spotifydiscoverplaylist', name: 'spotifydiscoverplaylist-page', component: SpotifyDiscoverPlaylist },
+    { path: '/portfolio/fargateenv',              name: 'fargateenv-page',              component: FargateEnv }
   ]
 })
